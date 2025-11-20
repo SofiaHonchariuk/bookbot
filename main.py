b6c3965b29770085f908466439b206db4a8f9675
SofiaHonchariuk <sofiahonchariuk@gmail.com>
2025-11-20 19:51:27 +0100

    words counter

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
+def count_words(text):
+    words = text.split()
+    number_of_words = 0 
+    for i in words:
+        number_of_words += 1
+    return(number_of_words)
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    text = get_book_text("books/frankenstein.txt")
+    num_words = count_words(text)
+    print(f"Found {num_words} total words") 
+  
 
 main()
